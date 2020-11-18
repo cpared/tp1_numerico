eec705b0e09554a469d633c695892017f8e75881
Raul Palavecino <rbpalavecino@bna.com.ar>
2020-11-18 02:13:10 -0300

    mejoras de fs de secante y normaliz de fs de N-R

--- newton_raphson.py
+++ newton_raphson.py
@@ -1,32 +1,82 @@
 """NEWTON RAPHSON FUNCION 1"""
 
 import numpy as np
+from scipy import optimize
+
 R = 4.25
 N = 6
 S = 20
 xi = 2
+
 def f(x):
-  return((np.pi*x**2*(3*R-x)/3)-(np.pi*4*R**3*S)/(3*N*9.5))
+    return (np.cos(x)-x)
 
 def df(x):
-  return(2*np.pi*x*R-np.pi*x**2)
+    return -np.sin(x) - 1
 
-def newton_raphson(f,df,xi):
-  n = 1
-  error = 1e5
-  x = xi
-  while error > 1e-8:
-    x = x-f(x)/df(x)
-    error = abs(f(x))
-    n += 1
-  print("La raiz es: {:.4}".format(x))
-  print("Las iteraciones son: {}".format(n))
+"""
+def f(x):
+    return((np.pi*x**2*(3*R-x)/3)-(np.pi*4*R**3*S)/(3*N*9.5))
+
+def df(x):
+    return(2*np.pi*x*R-np.pi*x**2)
+"""
+
+
+def newton_raphson_x_cota(f, df, p0, cota_max, mostrar_tabla = False):
+
+    if mostrar_tabla:
+        print("Iter:", 0, " Valor:", p0, "  ", "  |pn-pn-1|:---")
+    
+    pn = p0
+    i = 0
+    cota_error = cota_max + 1
+    
+    while cota_error > cota_max:
+        i+=1
+        pn_1 = pn
+        pn = pn_1 - f(pn_1) / df(pn_1)
+        cota_error = abs(pn - pn_1)
+        
+        if mostrar_tabla:
+            print("Iter:", i, " Valor:", pn, "  ", "  |pn-pn-1|:", cota_error)
+
+    return pn
+
+    
+        
+def newton_raphson_x_iteraciones(f, df, p0, iteraciones, mostrar_tabla = False):
+
+    if mostrar_tabla:
+        print("Iter:", 0, " Valor:", p0, "  ", "  |pn-pn-1|:---")
+    
+    pn = p0
+    
+    for i in range(1, iteraciones):
+
+        pn_1 = pn
+        pn = pn_1 - f(pn_1) / df(pn_1)
+        cota_error = abs(pn - pn_1)
+        
+        if mostrar_tabla:
+            print("Iter:", i, " Valor:", pn, "  ", "  |pn-pn-1|:", cota_error)
+
+    return pn
+
+
+
 def main():
-  newton_raphson(f,df,xi)
+    p0 = 0.785398163 #np.pi/4;
+    iteraciones = 6
+    root = newton_raphson_x_cota(f, df, p0, 1e-16, True)
+    print("Resultado Newton-Raphson:  ", root)
+    print("Resultado optimize.brentq: ", optimize.brentq(f, 0, 4))
+  
 main()
 
-"""NEWTON RAPHSON FUNCION 2"""
 
+"""NEWTON RAPHSON FUNCION 2"""
+"""
 import numpy as np
 R = 4.25
 N = 6
@@ -51,3 +101,5 @@ def nr_mod(f,df,xi):
 def main():
   nr_mod(f,df,xi)
 main()
+
+"""
--- secante.py
+++ secante.py
@@ -1,5 +1,6 @@
 """Secante (metodo controlado por iteracion y por cota)"""
 
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
@@ -8,15 +9,16 @@ R = 4.25    #Radio del tanque
 N = 6       #Cantidad de iontegrantes
 S = 20      #Suma ultimo digito del padron
 
-def f(x):
-  return((np.pi*x**2*(3*R-x)/3)-(np.pi*4*R**3*S)/(3*N*9.5))
+#def f(x):
+#    return((np.pi*x**2*(3*R-x)/3)-(np.pi*4*R**3*S)/(3*N*9.5))
 
-"""
+
 #Usar x = np.linspace(-np.pi, np.pi, 1000)
 #Rango para semilla por biseccion de 0 a np.pi/2
+#Semilla np.pi/4 y 0.5
 def f(x):
     return (np.cos(x)-x)
-"""
+
 
 def biseccion_x_iteracion(f, a, b, iteracion_max, mostrar_tabla=False):
 
@@ -31,7 +33,8 @@ def biseccion_x_iteracion(f, a, b, iteracion_max, mostrar_tabla=False):
         f_medio = f(medio)
         
         if mostrar_tabla:
-            print("Iter:{0}  |  a:{1}  |  b:{2}  |  f(a):{3}  |  f(b):{4}  |  f(medio){5}".format(i, a, b, f(a), f(b), f(medio)))
+            print("Iter:{0}   Valor:{1}   (bn - an)/2:{2}".format(i, f_medio, medio))
+            #print("Iter:{0}  |  a:{1}  |  b:{2}  |  f(a):{3}  |  f(b):{4}  |  f(medio){5}".format(i, a, b, f(a), f(b), f(medio)))
         
         #Verificacion del caso limite donde la raiz cae en algun extremo
         if f_a == 0: return a, i
@@ -51,46 +54,56 @@ def biseccion_x_iteracion(f, a, b, iteracion_max, mostrar_tabla=False):
 
 
 def secante_x_cota(f,p0, p1, cota_max, mostrar_tabla=False):
-    n = 0
-    cota = abs(p1 - p0)
-    p = p0
+    cota_error = abs(p1 - p0)
+
+    if mostrar_tabla:
+        print("Iter:", 0, "   Valor:", p0, "    |pn - pn-1|:---")
+        print("Iter:", 1, "   Valor:", p1, "    |pn - pn-1|:", cota_error)
+
+    iteracion = 1
+    pn = p1
+    pn_1 = p0
     
-    while cota > cota_max:
+    while cota_error > cota_max:        
+        iteracion += 1
+        pn_2 = pn_1
+        pn_1 = pn 
+        pn = pn_1 - f(pn_1)*(pn_1 - pn_2) / (f(pn_1) - f(pn_2)) 
+        cota_error = abs(pn - pn_1)
         
         if mostrar_tabla:
-            print("Iter:", n, " Valor:", p1, "  ", "  |pn-pn-1|:", cota )
+            print("Iter:", iteracion, "   Valor:", pn, "  |pn - pn-1|:", cota_error)
         
-        p = p0 - f(p0)*(p0-p1) / (f(p0)-f(p1)) 
-        cota = abs(p1 - p0)
-        p1 = p0
-        p0 = p
-        n += 1
-        
-    return p
+    return pn
 
 
 def secante_x_iteracion(f,p0, p1, iteracion_max, mostrar_tabla=False):
-    n = 0
-    cota = abs(p1 - p0)
-    p = p0
-    
-    for i in range(1, iteracion_max+1):
-        
-        if mostrar_tabla:
-            print("Iter:", n, " Valor:", p1, "  ", "  |pn-pn-1|:", cota )
-        
-        p = p0 - f(p0)*(p0-p1) / (f(p0)-f(p1)) 
-        cota = abs(p1 - p0)
-        p1 = p0
-        p0 = p
-        n += 1
+    cota_error = abs(p1 - p0)
 
-    return p
+    if mostrar_tabla:
+        print("Iter:", 0, "   Valor:", p0, "    |pn - pn-1|:---")
+        print("Iter:", 1, "   Valor:", p1, "    |pn - pn-1|:", cota_error)
+
+    pn = p1
+    pn_1 = p0
+
+    for i in range(2, iteracion_max):
+        
+        pn_2 = pn_1
+        pn_1 = pn
+        pn = pn_1 - f(pn_1)*(pn_1 - pn_2) / (f(pn_1) - f(pn_2)) 
+        
+        cota_error = abs(pn - pn_1)
+        if mostrar_tabla:
+            print("Iter:", i, "   Valor:", pn, "   |pn-pn-1|:", cota_error)
+    
+    return pn
 
 
 def main():
     #Graifco la funcion pedida
-    x = np.linspace(-np.pi, 5, 1000)
+    # x = np.linspace(-np.pi, 5, 1000)
+    x = np.linspace(-np.pi, np.pi, 1000)
     plt.plot(x, f(x), "blue")
     plt.show()
     
@@ -101,14 +114,14 @@ def main():
     p1, i = biseccion_x_iteracion(f, 0, 4, 2)
     print("P0={0}   |   P1={1}".format(p0, p1))
 
-    #Con una cota menor a 1e-15, el denominador del cociente de la 
+    #Con una cota menor a 1e-14, el denominador del cociente de la 
     #aproximacion de la derivada se va a infinito
-    root = secante_x_cota(f, np.pi/4, 0.5, 1e-15, True)
+    root = secante_x_cota(f, 0.5, 0.785398163, 1e-14, True)
     print("Secante x cota: ", root)
     
     #Con mas de 6 iteraciones (de 0 a 5), el denominador del cociente de la
     #aproximacion de la derivada se va a infinito
-    root = secante_x_iteracion(f, np.pi/4, 0.5, 8, True)     
+    root = secante_x_iteracion(f, 0.5, 0.785398163, 8, True)     
     print("Secante x iteracion: ", root)
 
 main()
